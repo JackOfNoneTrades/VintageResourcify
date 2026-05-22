e258b4bd59e8cfd6ea736fd0cdc39381a8f8c46d
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-05-22 16:58:57 +0200

    Added cancel button in resourcepack gui

--- src/main/java/dev/dediamondpro/resourcify/mixins/early/minecraft/MixinGuiScreenResourcePacks.java
+++ src/main/java/dev/dediamondpro/resourcify/mixins/early/minecraft/MixinGuiScreenResourcePacks.java
@@ -1,9 +1,13 @@
 package dev.dediamondpro.resourcify.mixins.early.minecraft;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiOptionButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
+import net.minecraft.client.resources.I18n;
 
+import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +18,43 @@ import dev.dediamondpro.resourcify.gui.pack.PackScreensAddition;
 import dev.dediamondpro.resourcify.services.ProjectType;
 
 @Mixin(GuiScreenResourcePacks.class)
-public class MixinGuiScreenResourcePacks {
+public class MixinGuiScreenResourcePacks extends GuiScreen {
+
+    private static final int DONE_BUTTON_ID = 1;
+    private static final int CANCEL_BUTTON_ID = -33701;
+    private static final int ACTION_BUTTON_WIDTH = 72;
+    private static final int ACTION_BUTTON_GAP = 6;
+
+    @Inject(method = "initGui", at = @At("TAIL"))
+    private void resourcify$addCancelButton(CallbackInfo ci) {
+        int cancelX = this.width / 2 + 4;
+        int doneX = cancelX + ACTION_BUTTON_WIDTH + ACTION_BUTTON_GAP;
+
+        for (GuiButton button : this.buttonList) {
+            if (button.id == DONE_BUTTON_ID) {
+                button.xPosition = doneX;
+                button.width = ACTION_BUTTON_WIDTH;
+                break;
+            }
+        }
+
+        this.buttonList.add(
+            new GuiOptionButton(
+                CANCEL_BUTTON_ID,
+                cancelX,
+                this.height - 48,
+                ACTION_BUTTON_WIDTH,
+                20,
+                I18n.format("gui.cancel", new Object[0])));
+    }
+
+    @Inject(method = "actionPerformed", at = @At("HEAD"), cancellable = true)
+    private void resourcify$cancelLikeEscape(GuiButton button, CallbackInfo ci) {
+        if (button.enabled && button.id == CANCEL_BUTTON_ID) {
+            this.keyTyped('\0', Keyboard.KEY_ESCAPE);
+            ci.cancel();
+        }
+    }
 
     @Inject(method = "drawScreen", at = @At("HEAD"))
     private void resourcify$resetDeleteRegions(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
