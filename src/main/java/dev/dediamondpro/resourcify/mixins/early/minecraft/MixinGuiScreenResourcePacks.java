d6886e5ed72c21e27cf351e7a5ed420feb2ded49
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-05-30 16:00:05 +0200

    Resource pack cancel button goes back to correct screen

--- src/main/java/dev/dediamondpro/resourcify/mixins/early/minecraft/MixinGuiScreenResourcePacks.java
+++ src/main/java/dev/dediamondpro/resourcify/mixins/early/minecraft/MixinGuiScreenResourcePacks.java
@@ -7,7 +7,6 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
 import net.minecraft.client.resources.I18n;
 
-import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -51,7 +50,7 @@ public class MixinGuiScreenResourcePacks extends GuiScreen {
     @Inject(method = "actionPerformed", at = @At("HEAD"), cancellable = true)
     private void resourcify$cancelLikeEscape(GuiButton button, CallbackInfo ci) {
         if (button.enabled && button.id == CANCEL_BUTTON_ID) {
-            this.keyTyped('\0', Keyboard.KEY_ESCAPE);
+            this.mc.displayGuiScreen(((PackScreenAccessor) (Object) this).getParentScreen());
             ci.cancel();
         }
     }
